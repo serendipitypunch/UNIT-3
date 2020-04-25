a31eee5b551c358b4b7eac5da265269039a4db79
serendipitypunch <michaeltsherry@gmail.com>
2020-04-24 21:19:03 -0700

    Removed ""
    I went through and cleaned up the code a bit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-const nameField = document.getElementById("name").focus(); //on page load, the cursor appears in the "Name" field, ready for a user to type
-const otherField = document.getElementById("other-title").style.display = "none"; //hides the "other-title" field I added to the HTML
+
+/*** BASIC INFO SECTION ***/
+const nameField = document.getElementById('name').focus(); //on page load, the cursor appears in the "Name" field, ready for a user to type
+const otherField = document.getElementById('other-title').style.display = 'none'; //hides the "other-title" field I added to the HTML
+
+
+
+// $("#title").change(function(){
+
+//     if (document.querySelector('#title option[value="other"]').selected = true){
+//         console.log('Unhide!!!!');
+//     }
+// });
 
 
 
 /*** T-SHIRT SECTION ***/
 const selectAShirt = document.querySelector('#design option').hidden = true;  //hides the "select theme" option in the "Design" menu.
-let colorSelect = document.getElementById("color"); //selects the color Select by ID and stores it in "colorSelect".
+let colorSelect = document.getElementById('color'); //selects the color Select by ID and stores it in "colorSelect".
 let colorOptions = colorSelect.options; //selects the option in the colorSelect dropdown menu
 
 $(colorOptions).hide(); //hides all the options
 
-const opt = document.createElement("option"); // creates an option element and stores it in "opt"
-opt.value = "selectshirt"; //sets the value of opt
-opt.text = "Please select a T-shirt theme"; //sets the text value of opt
+const opt = document.createElement('option'); // creates an option element and stores it in "opt"
+opt.value = 'selectshirt'; //sets the value of opt
+opt.text = 'Please select a T-shirt theme'; //sets the text value of opt
 opt.hidden = true;
 opt.selected = true;
 colorSelect.add(opt, colorSelect.options[0]); //adds opt to colorSelect as first child
@@ -20,12 +31,12 @@ colorSelect.add(opt, colorSelect.options[0]); //adds opt to colorSelect as first
 $('#design').change(function(){ //creates a click event that triggers when a change is made to the select element
     $(colorOptions).hide(); //clears the color options (basically a reset)
     const selectedValue = $('#design').val(); //stores the value of user selection in "selectedValue"
-    if (selectedValue === "js puns") { //if selectedValue equals "js puns" then
+    if (selectedValue === 'js puns') { //if selectedValue equals "js puns" then
     $(colorOptions[1]).show(); //show options 1-3
     $(colorOptions[2]).show();
     $(colorOptions[3]).show();
     colorOptions[1].selected = true; //sets the 1st option as selected
-    } else if (selectedValue === "heart js"){ //else if selectedValue equals "heart js" then
+    } else if (selectedValue === 'heart js'){ //else if selectedValue equals "heart js" then
         $(colorOptions[4]).show(); //show options 4-6
         $(colorOptions[5]).show();
         $(colorOptions[6]).show();
@@ -37,7 +48,8 @@ $('#design').change(function(){ //creates a click event that triggers when a cha
 /*** ACTIVITIES SECTION ***/
 
 let activityCostCounter = 0;
-let activityCostDiv = document.createElement("DIV"); //creates a new Div element and stores it in "activityCostDiv"
+let activityCostDiv = document.createElement('DIV'); //creates a new Div element and stores it in "activityCostDiv"
+activityCostDiv.style.paddingTop = '10px';
 let activityCost = document.createElement("P").textContent = 0; //creates a P element, sets it's value to 0 and stores it in "activityCost"
 activityCostDiv.append(activityCost); //appends activityCost to activityCostDiv
 document.querySelector(".activities").appendChild(activityCostDiv); //appends the activityCost Div to the .activities section
@@ -79,28 +91,26 @@ document.querySelector('.activities').addEventListener('change', (e) => { //crea
 
 
 
-
-
 /*** PAYMENT SECTION ***/
 document.querySelector('#payment option[value="select method"]').hidden = true; //hides the "Select Payment Method" option
 document.querySelector('#payment option[value="credit card"]').selected = true; //sets the "credit card" option as selected
 
-$("#paypal").hide(); //hides the paypal div
-$("#bitcoin").hide(); //hides the bitcoin div
+$('#paypal').hide(); //hides the paypal div
+$('#bitcoin').hide(); //hides the bitcoin div
 
 $("#payment").change(function(){
     const selectedPayment = $('#payment').val();
     if (selectedPayment === "credit card") { //if selectedPayment equals "credit card" then only show credit card div
-        $("#credit-card").show(); 
-        $("#paypal").hide();
-        $("#bitcoin").hide();
-    } else if (selectedPayment === "paypal"){ //else if selectedPayment equals "paypal" then only show paypal div
-        $("#credit-card").hide();
-        $("#paypal").show();
-        $("#bitcoin").hide();
+        $('#credit-card').show(); 
+        $('#paypal').hide();
+        $('#bitcoin').hide();
+    } else if (selectedPayment === 'paypal'){ //else if selectedPayment equals "paypal" then only show paypal div
+        $('#credit-card').hide();
+        $('#paypal').show();
+        $('#bitcoin').hide();
         } else { // else only show bitcoin
-            $("#credit-card").hide(); 
-            $("#paypal").hide();
-            $("#bitcoin").show();
+            $('#credit-card').hide(); 
+            $('#paypal').hide();
+            $('#bitcoin').show();
         }
 });
